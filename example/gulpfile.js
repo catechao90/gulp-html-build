cefab15d6362cd758329e4219664dfde8253657f
cate.zhao <cate.zhao@tpooo.com>
2016-03-09 15:11:49 +0800

    updated core logic

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,17 +1,9 @@
 var gulp = require('gulp');
-var htmlInsert = require('gulp-html-build').htmlInsert,
-    generateView = require('gulp-html-build').generateView;
+var htmlInsert = require('gulp-html-build').htmlInsert;
 
 
-gulp.task('default',['pubilc'], function() {
+gulp.task('default',function() {
   return gulp.src('src/*.html')
     .pipe(htmlInsert({src:"src/public/"}))    
     .pipe(gulp.dest('build'));
-});
-
-
-gulp.task('pubilc', function() {
-  return gulp.src('src/views/*.html')
-    .pipe(generateView({dest:"src/public/"}))    
-    .pipe(gulp.dest('src/public'));
 });
