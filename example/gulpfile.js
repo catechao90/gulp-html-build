2bb60a7ddb74599380e97a5bdc1cee9bc7a14dcd
cate.zhao <cate.zhao@tpooo.com>
2016-03-09 15:39:34 +0800

    create temp branch

--- example/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var htmlInsert = require('gulp-html-build').htmlInsert;
-
-
-gulp.task('default',function() {
-  return gulp.src('src/*.html')
-    .pipe(htmlInsert({src:"src/public/"}))    
-    .pipe(gulp.dest('build'));
-});
